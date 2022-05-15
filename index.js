f99193ebbb39f085e10161cb60e994ba39b9d507
farid5630 <mdforidulislam5630@gmail.com>
2022-05-15 17:31:08 +0600

    single product api complete

--- index.js
+++ index.js
@@ -1,15 +1,56 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
+const app = express();
+
+// middleware
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('this is server site code');
-})
+// warehouseStore:HNtDQJLJFfVcXu6F
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4zoya.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client
+      .db("warehouse")
+      .collection("stationary-product");
+
+    // products 
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+
+    // manage product list
+    app.get("/manage/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await productCollection.findOne(query);
+      res.send(product);
+    });
+
+  } finally {
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("this is server site code");
+});
 
 app.listen(port, () => {
-    console.log('this server is runing', port)
+  console.log("this server is runing", port);
 });
