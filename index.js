f1ff9ce5daeb8a54b9dc1ba8513874800abc84fe
farid5630 <mdforidulislam5630@gmail.com>
2022-05-17 00:36:16 +0600

    heroku

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// mongo db connection 
+// mongodb connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4zoya.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
